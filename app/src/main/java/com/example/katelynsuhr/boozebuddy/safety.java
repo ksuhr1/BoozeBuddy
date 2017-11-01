6f788949a12eae856a95dd89ecb8ed6701dc35ab
Katelyn Suhr <ksuhr1@me.com>
2017-11-01 15:40:35 -0700

    fixed all errors

--- app/src/main/java/com/example/katelynsuhr/boozebuddy/safety.java
+++ app/src/main/java/com/example/katelynsuhr/boozebuddy/safety.java
@@ -4,17 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.provider.ContactsContract;import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager.LoaderCallbacks;
-import android.widget.AdapterView;
+
 
 public class safety extends AppCompatActivity {
 
