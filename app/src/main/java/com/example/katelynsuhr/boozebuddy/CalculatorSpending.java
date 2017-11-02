bc1ad4f4a985651041459d548414968cfb14c747
Anisha Tavva <anishatavva@eduroam-169-233-195-234.ucsc.edu>
2017-11-02 11:42:06 -0700

    The main menu

---
+++ app/src/main/java/com/example/katelynsuhr/boozebuddy/CalculatorSpending.java
@@ -0,0 +1,13 @@
+package com.example.katelynsuhr.boozebuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorSpending extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator_spending);
+    }
+}
